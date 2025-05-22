36326e2a28ccd7af5ca233a7380b0d44ed357a93
DanielRRL <reinaramon289@gmail.com>
2025-05-21 21:17:37 -0500

    corretion deploy4

--- backend/performance-service/src/main/java/com/rh_systems/performance_service/PerformanceServiceApplication.java
+++ backend/performance-service/src/main/java/com/rh_systems/performance_service/PerformanceServiceApplication.java
@@ -2,8 +2,10 @@ package com.rh_systems.performance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PerformanceServiceApplication {
 
 	public static void main(String[] args) {
--- backend/schedule-service/src/main/java/com/rh_systems/schedule_service/ScheduleServiceApplication.java
+++ backend/schedule-service/src/main/java/com/rh_systems/schedule_service/ScheduleServiceApplication.java
@@ -2,8 +2,10 @@ package com.rh_systems.schedule_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ScheduleServiceApplication {
 
 	public static void main(String[] args) {
